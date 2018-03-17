aae49ec5a066106903f97e25f205cf1ebdef9a96
Tom VO <vanostra88@gmail.com>
2018-03-17 10:36:18 -0400

    Adding 'iris' Scikit script and associted files

--- sraval_pythonAI01.py
+++ sraval_pythonAI01.py
@@ -1,12 +1,19 @@
 from sklearn import tree
 
+def print_prediction(prediction):
+	for item in prediction:
+		print (item)
+
 X = [ [68,172,8.5], [72,225,12], [63,119,6], [70,162,10.5], [76,295,14], [60,102,5], [59,99,4.5], [71,187,10.5], [68,158,9], [68,142,7.5], [71,205,10.5]]
 Y = ['male', 'male', 'female', 'female', 'male', 'female', 'female', 'male', 'male', 'female', 'male']
 
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(X,Y)
-prediction = clf.predict([[68,165,9]])
 
-#print ("Prediction is: %s") %(prediction) 
-for item in prediction:
-	print (item)
+
+prediction = clf.predict([[68,165,9]])
+print_prediction(prediction)
+
+prediction = clf.predict([[62,125,6]])
+print_prediction(prediction)
+
---
+++ sraval_pythonAI_Iris.py
@@ -0,0 +1,22 @@
+from sklearn.datasets import load_iris
+from sklearn import tree
+
+import graphviz
+
+def print_prediction(prediction):
+	for item in prediction:
+		print (item)
+
+iris = load_iris()
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(iris.data, iris.target)
+
+
+dot_data = tree.export_graphviz(clf, out_file=None, 
+                         feature_names=iris.feature_names,  
+                         class_names=iris.target_names,  
+                         filled=True, rounded=True,  
+                         special_characters=True) 
+
+graph = graphviz.Source(dot_data) 
+graph.render("iris") 
